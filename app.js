1af79956319b6bda6bdbfd49d5d4b5f632d506f7
Ray <59398743+xiraynedev@users.noreply.github.com>
2020-05-01 02:01:08 -0400

    uploading Web component files

---
+++ app.js
@@ -0,0 +1,121 @@
+class Modal extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.isOpen = false;
+    this.shadowRoot.innerHTML = `
+        <style>
+            #backdrop {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100vh; 
+                background: rgba(0,0,0,.4);
+                z-index: 10;
+                opacity: 0;
+                pointer-events: none;
+            }
+            :host([opened]) #backdrop,
+            :host([opened]) #modal {
+                opacity: 1;
+                pointer-events: all;
+            }
+            #modal {
+                position: fixed;
+                top: 15vh;
+                left: 25%;
+                width: 50%;
+                z-index: 100;
+                background: white;
+                border-radius: 3px;
+                box-shadow: 0 2px 8px rgba(0,0,0.26);\
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                opacity: 0;
+                pointer-events: none;
+            }
+            header {
+                padding: 1rem;
+            }
+            header h1 {
+                font-size: 1.25rem;
+            }
+            #main {
+                padding: 1rem;
+            }
+            #actions {
+                border-top: 1px solid #ccc;
+                padding: 1rem;
+                display: flex;
+                justify-content: flex-end;
+            }
+
+            #actions button {
+                margin: 0 .25rem;
+            }
+        </style>
+        
+        <div id="backdrop"></div>
+            <div id="modal">
+                <header>
+                    <slot name="title">Please Confirm</slot>
+                </header>
+                <section id="main">
+                    <slot></slot>
+                </section>
+                <section id="actions">
+                    <button id="cancel-btn">Cancel</button>
+                    <button id="confirm-btn">Okay</button>
+                </section>
+            </div>
+        `;
+    const slots = this.shadowRoot.querySelectorAll('slot');
+    slots[1].addEventListener('slotchange', (event) => {
+      console.dir(slots[1].assignedNodes());
+    });
+
+    const backdrop = this.shadowRoot.querySelector('#backdrop');
+    backdrop.addEventListener('click', this._cancel.bind(this));
+
+    const cancelButton = this.shadowRoot.querySelector('#cancel-btn');
+    const confirmButton = this.shadowRoot.querySelector('#confirm-btn');
+
+    cancelButton.addEventListener('click', this._cancel.bind(this));
+    confirmButton.addEventListener('click', this._confirm.bind(this));
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.hasAttribute('opened')) {
+      this.isOpen = true;
+    } else {
+      this.isOpen = false;
+    }
+  }
+
+  open() {
+    this.isOpen = true;
+    this.setAttribute('opened', '');
+  }
+
+  hide() {
+    if (this.hasAttribute('opened')) {
+      this.removeAttribute('opened');
+      this.isOpen = false;
+    }
+  }
+
+  _cancel(e) {
+    this.hide();
+    const cancelEvent = new Event('cancel', { bubbles: true, composed: true });
+    e.target.dispatchEvent(cancelEvent);
+  }
+
+  _confirm(e) {
+    this.hide();
+    const confirmEvent = new Event('confirm');
+    this.dispatchEvent(confirmEvent);
+  }
+}
+
+customElements.define('uc-modal', Modal);
